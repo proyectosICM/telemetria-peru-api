c2918136e26691841ac7a149d2d79cb1c1b70e0a
Eduardo Aguilar <es123y123@gmail.com>
2024-12-02 10:09:04 -0500

    c

--- src/main/java/com/icm/telemetria_peru_api/controllers/FuelEfficiencyController.java
+++ src/main/java/com/icm/telemetria_peru_api/controllers/FuelEfficiencyController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -35,7 +36,7 @@ public class FuelEfficiencyController {
             @PathVariable Long vehicleModelId,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "8") int size) {
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
         Page<FuelEfficiencyModel> data = fuelEfficiencyService.findByVehicleModelId(vehicleModelId, pageable);
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
