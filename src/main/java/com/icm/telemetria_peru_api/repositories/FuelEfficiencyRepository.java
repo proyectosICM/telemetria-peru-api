4d4a5a1a1bac9d5095ec663ec74d192127995bd6
Eduardo Aguilar <es123y123@gmail.com>
2024-12-17 17:43:50 -0500

    c

--- src/main/java/com/icm/telemetria_peru_api/repositories/FuelEfficiencyRepository.java
+++ src/main/java/com/icm/telemetria_peru_api/repositories/FuelEfficiencyRepository.java
@@ -27,25 +27,25 @@ public interface FuelEfficiencyRepository extends JpaRepository<FuelEfficiencyMo
      * STATS
      */
     @Query(value = """
-            WITH RECURSIVE dates AS (
-                SELECT DATE_FORMAT(DATE(CONCAT(YEAR(CURDATE()), '-', :month, '-01')), '%Y-%m-%d') AS day
-                UNION ALL
-                SELECT DATE_ADD(day, INTERVAL 1 DAY)
-                FROM dates
-                WHERE day < CURDATE() -- Limitar solo hasta la fecha actual
-            )
-            SELECT 
-                d.day AS day,
-                IFNULL(AVG(fe.fuel_efficiency), 0) AS averageValue
-            FROM dates d
-            LEFT JOIN fuel_efficiency fe 
-                ON DATE(CONVERT_TZ(fe.created_at, '+00:00', '-05:00')) = d.day
-                AND MONTH(CONVERT_TZ(fe.created_at, '+00:00', '-05:00')) = :month
-                AND YEAR(CONVERT_TZ(fe.created_at, '+00:00', '-05:00')) = YEAR(CURDATE())
-                AND fe.vehicle_id = :vehicleId
-            GROUP BY d.day
-            ORDER BY d.day
-            """, nativeQuery = true)
+    WITH RECURSIVE dates AS (
+        SELECT DATE_FORMAT(DATE(CONCAT(YEAR(CURDATE()), '-', :month, '-01')), '%Y-%m-%d') AS day
+        UNION ALL
+        SELECT DATE_ADD(day, INTERVAL 1 DAY)
+        FROM dates
+        WHERE day < LAST_DAY(CONCAT(YEAR(CURDATE()), '-', :month, '-01'))
+    )
+    SELECT 
+        d.day AS day,
+        IFNULL(AVG(fe.fuel_efficiency), 0) AS averageValue
+    FROM dates d
+    LEFT JOIN fuel_efficiency fe 
+        ON DATE(CONVERT_TZ(fe.created_at, '+00:00', '-05:00')) = d.day
+        AND MONTH(CONVERT_TZ(fe.created_at, '+00:00', '-05:00')) = :month
+        AND YEAR(CONVERT_TZ(fe.created_at, '+00:00', '-05:00')) = YEAR(CURDATE())
+        AND fe.vehicle_id = :vehicleId
+    GROUP BY d.day
+    ORDER BY d.day
+    """, nativeQuery = true)
     List<Map<String, Object>> findDailyAveragesForMonth(@Param("vehicleId") Long vehicleId, @Param("month") Integer month);
 
 
