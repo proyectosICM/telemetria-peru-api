cb4ab610470e9c612602b75e7df2976728fc2d92
Eduardo Aguilar <es123y123@gmail.com>
2024-12-18 14:21:18 -0500

    c

--- src/main/java/com/icm/telemetria_peru_api/repositories/FuelEfficiencyRepository.java
+++ src/main/java/com/icm/telemetria_peru_api/repositories/FuelEfficiencyRepository.java
@@ -57,7 +57,7 @@ public interface FuelEfficiencyRepository extends JpaRepository<FuelEfficiencyMo
                 FROM fuel_efficiency fe
                 WHERE fe.vehicle_id = :vehicleId
                   AND YEAR(CONVERT_TZ(fe.created_at, '+00:00', '-05:00')) = :year  -- Usamos el parámetro :year
-                  AND status = :status
+                  AND fe.status = :status
                 GROUP BY DATE_FORMAT(CONVERT_TZ(fe.created_at, '+00:00', '-05:00'), '%Y-%m')
                 ORDER BY month
             """, nativeQuery = true)
