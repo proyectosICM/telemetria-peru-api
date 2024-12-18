9ee990496cfa78db57117e82895c6d37b396e521
Eduardo Aguilar <es123y123@gmail.com>
2024-12-18 15:00:06 -0500

    c

--- src/main/java/com/icm/telemetria_peru_api/repositories/FuelEfficiencyRepository.java
+++ src/main/java/com/icm/telemetria_peru_api/repositories/FuelEfficiencyRepository.java
@@ -50,25 +50,16 @@ public interface FuelEfficiencyRepository extends JpaRepository<FuelEfficiencyMo
     List<Map<String, Object>> findDailyAveragesForMonth(@Param("vehicleId") Long vehicleId, @Param("month") Integer month, @Param("year") Integer year);
 
     @Query(value = """
-            WITH RECURSIVE dates AS (
-                SELECT DATE_FORMAT(DATE(CONCAT(:year, '-', :month, '-01')), '%Y-%m-%d') AS day
-                UNION ALL
-                SELECT DATE_ADD(day, INTERVAL 1 DAY)
-                FROM dates
-                WHERE day < LAST_DAY(CONCAT(:year, '-', :month, '-01'))
-            )
-            SELECT\s
-                d.day AS day,
-                IFNULL(AVG(CASE WHEN fe.fuel_efficiency > 0 THEN fe.fuel_efficiency ELSE NULL END), 0) AS avgkm,
-                IFNULL(AVG(CASE WHEN fe.fuel_consumption_per_hour > 0 THEN fe.fuel_consumption_per_hour ELSE NULL END), 0) AS avgh
-            FROM dates d
-            LEFT JOIN fuel_efficiency fe\s
-                ON DATE(CONVERT_TZ(fe.created_at, '+00:00', '-05:00')) = d.day
-                AND MONTH(CONVERT_TZ(fe.created_at, '+00:00', '-05:00')) = :month
-                AND YEAR(CONVERT_TZ(fe.created_at, '+00:00', '-05:00')) = :year
-                AND fe.vehicle_id = :vehicleId
-            GROUP BY d.day
-            ORDER BY d.day;
+                SELECT 
+                    DATE_FORMAT(CONVERT_TZ(fe.created_at, '+00:00', '-05:00'), '%Y-%m') AS month,
+                    AVG(fe.fuel_efficiency) AS avgkm,
+                    AVG(fe.fuel_consumption_per_hour) AS avgh
+                FROM fuel_efficiency fe
+                WHERE fe.vehicle_id = :vehicleId
+                  AND YEAR(CONVERT_TZ(fe.created_at, '+00:00', '-05:00')) = :year  -- Usamos el parámetro :year
+                  AND status = :status
+                GROUP BY DATE_FORMAT(CONVERT_TZ(fe.created_at, '+00:00', '-05:00'), '%Y-%m')
+                ORDER BY month
             """, nativeQuery = true)
     List<Map<String, Object>> findMonthlyAveragesForYear(@Param("vehicleId") Long vehicleId, @Param("status") String status, @Param("year") Integer year);
 }
