7da800fecebaa7dca8099e64909eb2bb129e780a
Eduardo Aguilar <es123y123@gmail.com>
2024-12-26 17:44:25 -0500

    c

--- src/main/java/com/icm/telemetria_peru_api/dto/FuelEfficiencyDTO.java
+++ src/main/java/com/icm/telemetria_peru_api/dto/FuelEfficiencyDTO.java
@@ -4,9 +4,14 @@ import com.icm.telemetria_peru_api.enums.FuelType;
 import com.icm.telemetria_peru_api.models.FuelEfficiencyModel;
 import lombok.Data;
 
+import java.time.ZonedDateTime;
+
 @Data
 public class FuelEfficiencyDTO {
     private Long id;
+    private ZonedDateTime startTime;
+    private ZonedDateTime endTime;
+    private Double accumulatedHours;
     private String licensePlate;
     private String fuelEfficiencyStatus;
     private Double initialFuel;
@@ -15,14 +20,17 @@ public class FuelEfficiencyDTO {
     private Double fuelEfficiency;
     private Double fuelConsumptionPerHour;
 
+
     // Constructor
     public FuelEfficiencyDTO(FuelEfficiencyModel model) {
         this.id = model.getId();
+        this.startTime = model.getStartTime();
+        this.endTime= model.getEndTime();
+        this.accumulatedHours = model.getAccumulatedHours();
         this.licensePlate = model.getVehicleModel().getLicensePlate();
         this.fuelEfficiencyStatus = model.getFuelEfficiencyStatus().name();
         this.initialFuel = model.getInitialFuel();
         this.finalFuel = model.getFinalFuel();
-
         this.fuelType = model.getVehicleModel().getFuelType();
         this.fuelEfficiency = model.getFuelEfficiency();
         this.fuelConsumptionPerHour = model.getFuelConsumptionPerHour();
@@ -35,6 +43,8 @@ public class FuelEfficiencyDTO {
         } else {
             this.initialFuel = this.initialFuel;
             this.finalFuel = this.finalFuel;
+            this.fuelEfficiency = this.fuelEfficiency;
+            this.fuelConsumptionPerHour = this.fuelConsumptionPerHour;
         }
     }
 }
