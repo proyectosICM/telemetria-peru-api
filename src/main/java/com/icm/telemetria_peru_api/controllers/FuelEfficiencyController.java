9535ed72966dfa1eacea5c10b1e92b9623f0d672
Eduardo Aguilar <es123y123@gmail.com>
2025-01-15 16:13:21 -0500

    c

--- src/main/java/com/icm/telemetria_peru_api/controllers/FuelEfficiencyController.java
+++ src/main/java/com/icm/telemetria_peru_api/controllers/FuelEfficiencyController.java
@@ -14,7 +14,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+
 import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
