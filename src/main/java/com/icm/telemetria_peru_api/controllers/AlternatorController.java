cd9fab3416513a908598e04610fe4110a8d0fd8f
Eduardo Aguilar <es123y123@gmail.com>
2025-01-17 10:56:02 -0500

    c

--- src/main/java/com/icm/telemetria_peru_api/controllers/AlternatorController.java
+++ src/main/java/com/icm/telemetria_peru_api/controllers/AlternatorController.java
@@ -12,15 +12,12 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@RestController
 @RequestMapping("api/alternator")
 @RequiredArgsConstructor
 public class AlternatorController {
