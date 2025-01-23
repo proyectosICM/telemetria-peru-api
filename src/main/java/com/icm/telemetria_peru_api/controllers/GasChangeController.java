315af7ec926aab1c314ee1a296a01be3f3dac3e8
Eduardo Aguilar <es123y123@gmail.com>
2025-01-23 18:16:52 -0500

    c

--- src/main/java/com/icm/telemetria_peru_api/controllers/GasChangeController.java
+++ src/main/java/com/icm/telemetria_peru_api/controllers/GasChangeController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -49,7 +50,7 @@ public class GasChangeController {
                                                                      @RequestParam(defaultValue = "0") int page,
                                                                      @RequestParam(defaultValue = "10") int size) {
         try {
-            Pageable pageable = PageRequest.of(page, size);
+            Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
             Page<GasChangeModel> dataModel = gasChangeService.findByVehicleModelId(vehicleId, pageable);
 
             if (dataModel.isEmpty()) {
