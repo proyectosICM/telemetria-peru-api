21ccb821309191bceff13ed71a28bb01a10b29f1
Eduardo Aguilar <es123y123@gmail.com>
2025-02-03 13:14:10 -0500

    c

--- src/main/java/com/icm/telemetria_peru_api/services/EmailServiceImpl.java
+++ src/main/java/com/icm/telemetria_peru_api/services/EmailServiceImpl.java
@@ -5,6 +5,7 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -34,11 +35,16 @@ public class EmailServiceImpl implements IEmailRepository {
     }
 
     public void sendSms(String numero, String mensaje) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(numero + "@movistar.pe");
-        mailMessage.setSubject("SMS");
-        mailMessage.setText(mensaje);
-        mailSender.send(mailMessage);
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setTo(numero + "@movistar.pe");
+            mailMessage.setSubject("SMS");
+            mailMessage.setText(mensaje);
+            mailSender.send(mailMessage);
+        } catch (MailException e){
+            System.err.println("Error enviando SMS: " + e.getMessage());
+            throw new RuntimeException("No se pudo enviar el SMS", e);
+        }
     }
 
     @Override
