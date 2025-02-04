8925934a5e7678c6d5fd5ce4c36aee81345a6f43
Eduardo Aguilar <es123y123@gmail.com>
2025-02-04 16:16:35 -0500

    c

--- src/main/java/com/icm/telemetria_peru_api/services/SpeedExcessLoggerService.java
+++ src/main/java/com/icm/telemetria_peru_api/services/SpeedExcessLoggerService.java
@@ -1,19 +1,29 @@
 package com.icm.telemetria_peru_api.services;
 
+import com.icm.telemetria_peru_api.models.CompanyModel;
 import com.icm.telemetria_peru_api.models.SpeedExcessLoggerModel;
+import com.icm.telemetria_peru_api.models.UserModel;
+import com.icm.telemetria_peru_api.models.VehicleModel;
 import com.icm.telemetria_peru_api.repositories.SpeedExcessLoggerRepository;
+import com.icm.telemetria_peru_api.repositories.UserRepository;
+import com.icm.telemetria_peru_api.repositories.VehicleRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SpeedExcessLoggerService {
     private final SpeedExcessLoggerRepository speedExcessLoggerRepository;
+    private final VehicleRepository vehicleRepository;
+    private final UserRepository userRepository;
+    private final EmailServiceImpl emailService;
 
     public List<SpeedExcessLoggerModel> findAll(){
         return speedExcessLoggerRepository.findAll();
@@ -38,7 +48,39 @@ public class SpeedExcessLoggerService {
 
     /** More CRUD methods **/
     public SpeedExcessLoggerModel save(SpeedExcessLoggerModel speedExcessLoggerModel){
+        sendEmailInfo(speedExcessLoggerModel.getVehicleModel().getId());
         return speedExcessLoggerRepository.save(speedExcessLoggerModel);
+    }
+
+    public void sendEmailInfo(Long vehicleId) {
+        Optional<VehicleModel> vehicleModelOptional = vehicleRepository.findById(vehicleId);
+
+        if (vehicleModelOptional.isPresent()) {
+            VehicleModel vehicleModel = vehicleModelOptional.get();
+
+            CompanyModel companyModel = vehicleModel.getCompanyModel();
+            List<UserModel> userModels = userRepository.findByCompanyModelId(companyModel.getId());
+
+            String[] emails = userModels.stream()
+                    .map(UserModel::getEmail)
+                    .toArray(String[]::new);
+
+            if (emails.length > 0) {
+                String subject = "Velocidad maxima exedida ";
+                String message = "El vehículo con ID " + vehicleModelOptional.get().getLicensePlate() + " A excedido la velocidad máxima permitida.";
+
+                try {
+                    emailService.sendEmail(emails, subject, message);
+                    System.out.println("Mail sent to: " + Arrays.toString(emails));
+                } catch (Exception e) {
+                    System.err.println("Error sending email: " + e.getMessage());
+                }
+            } else {
+                System.out.println("No users associated with the vehicle company were found.");
+            }
+        } else {
+            System.out.println("A vehicle with the provided ID was not found.");
+        }
     }
 
     public void deleteById(Long id){
