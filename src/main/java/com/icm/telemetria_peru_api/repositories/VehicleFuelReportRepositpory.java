1bea181b801da48a8f8e709665f496cdc1cf1f68
Eduardo Aguilar <es123y123@gmail.com>
2025-05-27 09:49:32 -0500

    c

--- src/main/java/com/icm/telemetria_peru_api/repositories/VehicleFuelReportRepositpory.java
+++ src/main/java/com/icm/telemetria_peru_api/repositories/VehicleFuelReportRepositpory.java
@@ -21,9 +21,9 @@ public interface VehicleFuelReportRepositpory extends JpaRepository<VehicleFuelR
     @Query(value = """
     SELECT 
         AVG(initial_fuel - final_fuel) AS averageFuelConsumption,
-        SUM(TIME_TO_SEC(idle_time)) AS totalIdleTime,
-        SUM(TIME_TO_SEC(parked_time)) AS totalParkedTime,
-        SUM(TIME_TO_SEC(operating_time)) AS totalOperatingTime
+        SUM(idle_time) AS totalIdleTime,
+        SUM(parked_time) AS totalParkedTime,
+        SUM(operating_time) AS totalOperatingTime
     FROM vehicle_fuel_report
     WHERE (:vehicleId IS NULL OR vehicle_model_id = :vehicleId)
       AND (:year IS NULL OR YEAR(created_at) = :year)
