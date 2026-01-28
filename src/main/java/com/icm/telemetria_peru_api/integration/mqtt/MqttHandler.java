5a319132a2081fb81c46d38f7df07dda409f4bd8
Eduardo Aguilar <es123y123@gmail.com>
2026-01-28 14:27:38 -0500

    c

--- src/main/java/com/icm/telemetria_peru_api/integration/mqtt/MqttHandler.java
+++ src/main/java/com/icm/telemetria_peru_api/integration/mqtt/MqttHandler.java
@@ -135,7 +135,7 @@ public class MqttHandler {
         executeSafely(() -> gasRecordHandler.saveGasRecordModel(data, vehicle), "gasRecordHandler.analyzeFuelTimestamp");
         executeSafely(() -> alarmHandler.saveAlarmRecord(vehicle, data.getAlarmInfo()), "alarmHandler.saveAlarmRecord");
         executeSafely(() -> ignitionHandler.updateIgnitionStatus(vehicle, data.getIgnitionInfo()), "ignitionHandler.updateIgnitionStatus");
-        executeSafely(() -> fuelEfficiencyDailyHandler.process(vehicle, data), "fuelEfficiencyDailyHandler.process");
+        //executeSafely(() -> fuelEfficiencyDailyHandler.process(vehicle, data), "fuelEfficiencyHandler.processFuelEfficiencyInfo");
         executeSafely(() -> speedExcessHandler.logSpeedExcess(vehicle, data), "speedExcessHandler.logSpeedExcess");
         executeSafely(() -> vehicleSnapshotHandler.saveVehicleSnapshot(snapshotDTO,vehicle), "VehicleSnapshotHandler.saveVehicleSnapshot");
     }
